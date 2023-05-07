e3ec7ef5e4a589b2e8b57340690776b69e940b39
alireza <comolestyrusistantaneos@gmail.com>
2023-05-07 23:13:56 +0330

    first

---
+++ src/components/skills.component.tsx
@@ -0,0 +1,12 @@
+const Skills = () => {
+  return (
+    <div>
+      <h1>My Skills</h1>
+      <div>
+        tools: <div>typescript</div>
+        dom: <div>dom</div>
+      </div>
+    </div>
+  );
+};
+export default Skills;
---
+++ src/components/skillsIcon.component.tsx
@@ -0,0 +1,8 @@
+const SkillsIcon = ({ svgIcon }: Element) => {
+  <div className="bg-blue-900 p-4 m-1">
+    {svgIcon}
+    <div></div>
+  </div>;
+};
+
+export default SkillsIcon;
