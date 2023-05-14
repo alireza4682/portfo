e8b9752aac6cce74ff6df74571efca2a2d837cd5
alireza <comolestyrusistantaneos@gmail.com>
2023-05-14 16:19:51 +0330

    making skill buttons

--- src/components/skills.component.tsx
+++ src/components/skills.component.tsx
@@ -1,12 +1,23 @@
 import SkillsIcon from "./skillsIcon.component";
+
+import typescriptLogo from "../assets/typescript-icon.svg";
 const Skills = () => {
   return (
     <div>
       <h1>My Skills</h1>
       <div className="">
-        tools:{" "}
+        tools:
         <div>
-          <SkillsIcon svgIcon={<div>svgHere</div>} name={"iconName here"} />
+          <SkillsIcon
+            svgIcon={
+              <img
+                src={typescriptLogo}
+                alt="typescriptLogo"
+                className="bg-white text-black p-1.5 rounded-md m-2 w-16 h-16"
+              />
+            }
+            name={"typescript"}
+          />
         </div>
         dom: <div>dom</div>
       </div>
--- src/components/skillsIcon.component.tsx
+++ src/components/skillsIcon.component.tsx
@@ -1,6 +1,16 @@
-const SkillsIcon = ({ svgIcon, name }: any) => {
+import { ReactNode } from "react";
+
+export type TsvgIcon = {
+  svgIcon: ReactNode;
+  name: string;
+};
+
+const SkillsIcon = ({ svgIcon, name }: TsvgIcon) => {
   return (
-    <div className="bg-blue-900 p-4 m-1 flex ">
+    <div
+      className="bg-blue-900 p-1 px-2 m-1 
+                  flex flex-row justify-between w-48 items-center rounded-2xl"
+    >
       {svgIcon}
       <div className="px-1">{name}</div>
     </div>
